dd6ab6cefa4488acff3a3dcbfb1e69b2682f5ae0
Koki <kokicodes@gmail.com>
2023-12-24 18:54:11 -0500

    added a notification button for interns

--- views/src/components/FeedbackCard.jsx
+++ views/src/components/FeedbackCard.jsx
@@ -1,10 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { Text, Button, Paper, Container, Stack } from "@mantine/core";
+import { IconBell } from "@tabler/icons-react";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 import formatCreatedAt from "../Utility/DateFormatter";
+import checkTimeLapse from "../Utility/DateCompare";
 import { useDispatch } from "react-redux";
-import { assignFeedbackRequest, markComplete } from "../features/Feedbacks/feedbackSlice";
+import {
+  assignFeedbackRequest,
+  markComplete,
+} from "../features/Feedbacks/feedbackSlice";
 
 const feedbackContainer = {
   zIndex: "1",
@@ -23,11 +28,10 @@ function FeedbackCard({
   isLoading,
 }) {
   const [assignedRequests, setAssignedRequests] = useState([]);
+  const [notificationStatus, setNotificationStatus] = useState({});
   const [items, setItems] = useState(data);
   const navigate = useNavigate();
   const dispatch = useDispatch();
-
-  // console.log(data)
 
   const toggleComplete = (id) => {
     const updatedItems = items.map((item) => {
@@ -40,7 +44,7 @@ function FeedbackCard({
     const filteredItems = updatedItems.filter((item) => !item.completed);
 
     setItems(filteredItems);
-    dispatch(markComplete(id)); 
+    dispatch(markComplete(id));
   };
 
   const assignRequest = (id) => {
@@ -51,13 +55,26 @@ function FeedbackCard({
     }
   };
 
-
   const handleGoToDashboard = () => {
     const isMentor = true;
     if (isMentor) {
       navigate("/mentor/feedbackqueue");
     } else {
       navigate("/intern/myrequests");
+    }
+  };
+
+  const handleNotifyMentor = (itemId, index) => {
+    if (!notificationStatus[itemId]) {
+      setNotificationStatus((prevStatus) => ({
+        ...prevStatus,
+        [itemId]: true,
+      }));
+    }
+    // this would be the dispatch data
+    let placeholderData = {
+      topicOfLearningSession: items[index].topicOfLearningSession,
+      codeLink: items[index].codeLink
     }
   };
 
@@ -84,29 +101,55 @@ function FeedbackCard({
                 ) : (
                   <Text>Intern Name: {item.studentName}</Text>
                 )}
-                <Text>Topic Of Learning Session: {item.topicOfLearningSession}</Text>
+                <Text>
+                  Topic Of Learning Session: {item.topicOfLearningSession}
+                </Text>
                 <Text>Completed: {item.status ? "Yes" : "No"}</Text>
                 <Text>
                   Link to exercise: <a href={item.codeLink}>{item.codeLink}</a>
                 </Text>
                 <Text>{formatCreatedAt(item.createdAt)}</Text>
-                {item.whoisAssigned ? <Text>Assigned to: {item.whoisAssigned}</Text> : null}
+                {item.whoisAssigned ? (
+                  <Text>Assigned to: {item.whoisAssigned}</Text>
+                ) : null}
               </div>
-              <Stack direction="horizontal" spacing="sm">
+              <Stack
+                direction="horizontal"
+                spacing="sm"
+                style={{ justifyContent: "space-between", width: "160px" }}
+              >
                 {showAddFeedback && (
                   <Link to={`/feedback/${item.id}`}>
-                    <Button style={{ color: "black" }} color="#F9EB02">
+                    <Button
+                      style={{ color: "black", width: "100%" }}
+                      color="#F9EB02"
+                    >
                       Add Feedback
                     </Button>
                   </Link>
                 )}
                 {showViewFeedback && (
                   <Link to={`/feedback/${item.id}`}>
-                    <Button style={{ color: "black" }} color="#F9EB02">
+                    <Button style={{ color: "black", width: "100%" }} color="#F9EB02">
                       View Feedback
                     </Button>
                   </Link>
                 )}
+                {/* notification button location.pathname for testing*/}
+                {location.pathname === "/intern/myrequests" &&
+                  checkTimeLapse(item.createdAt) && (
+                    <Button
+                      onClick={() => handleNotifyMentor(item.id, index)}
+                      disabled={notificationStatus[item.id]}
+                      leftSection={<IconBell size={14} />}
+                      style={{ color: "black" }}
+                      color="#F9EB02"
+                    >
+                      {notificationStatus[item.id]
+                        ? "Mentor Notified"
+                        : "Notify Mentor"}
+                    </Button>
+                  )}
                 {showComplete && !item.completed && (
                   <Button
                     style={{ color: "black" }}
@@ -117,7 +160,11 @@ function FeedbackCard({
                   </Button>
                 )}
                 {gotoDashboard && (
-                  <Button style={{ color: "black" }} color="#F9EB02" onClick={handleGoToDashboard}>
+                  <Button
+                    style={{ color: "black" }}
+                    color="#F9EB02"
+                    onClick={handleGoToDashboard}
+                  >
                     Go to Dashboard
                   </Button>
                 )}
