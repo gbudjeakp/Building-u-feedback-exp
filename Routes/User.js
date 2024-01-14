579ca28c100f9e1851ac2b2fec2686e8b8ba8cbb
gbudjeakp <gbudjeakp@gmail.com>
2024-01-13 23:30:09 -0600

    Add auth route

--- Routes/User.js
+++ Routes/User.js
@@ -2,7 +2,10 @@ const express = require("express");
 const router =  express.Router();
 const userController   = require('../Controllers/userController')
 const rateLimiter = require("../middleware/rateLimiter");
+const auth = require("../middleware/auth")
 
+
+router.get("/authorized", auth, userController.authorized);
 router.get("/logout", userController.logout);
 router.post("/login",  rateLimiter, userController.loginUser);
 router.post("/register", rateLimiter, userController.registerUser);
