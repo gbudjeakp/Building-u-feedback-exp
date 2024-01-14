e703acae8fbd981649ddd8be373b3a8e835491d0
gbudjeakp <gbudjeakp@gmail.com>
2024-01-13 18:36:21 -0600

    Add input validation for update password controller, changed verbiagefor input validators

--- utility/inputValidator/loginValidator.js
+++ utility/inputValidator/loginValidator.js
@@ -20,7 +20,7 @@ module.exports = (input) => {
   }
 
   if (input.password.length < 8) {
-    errors.password = "Password must be at least 6 characters long";
+    errors.password = "Password must be at least 8 characters long";
   }
 
   if (input.password.length > 30) {
