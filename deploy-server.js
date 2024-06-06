283b13abf4ea6f4424ced84202c824a93bc6784c
gbudjeakp <gbudjeakp@gmail.com>
2024-06-06 00:21:54 -0500

    experimental deploy 1

---
+++ deploy-server.js
@@ -0,0 +1,21 @@
+const FtpDeploy = require('ftp-deploy');
+const path = require('path');
+
+const ftpDeploy = new FtpDeploy();
+
+const config = {
+  user: process.env.FTP_USER,
+  password: process.env.FTP_PASSWORD,
+  host: process.env.FTP_HOST,
+  port: 21,
+  localRoot: path.join(__dirname),
+  remoteRoot: '/public_html/building-u-feedback-server', 
+  include: ['*', '**/*'], 
+  exclude: ['.git', '.github', 'views/**', 'node_modules/**', 'deploy-backend.js', '.env'],
+  deleteRemote: false,
+  forcePasv: true,
+};
+
+ftpDeploy.deploy(config)
+  .then(res => console.log('Finished:', res))
+  .catch(err => console.error('Error:', err));
