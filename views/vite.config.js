3e29e4b1178d7e4d45fc5f48dad14c61cb9bc86f
gbudjeakp <gbudjeakp@gmail.com>
2024-06-06 01:36:22 -0500

    Tweak vite config

--- views/vite.config.js
+++ views/vite.config.js
@@ -1,12 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    watch: {
-      usePolling: true,
-    },
-  },
+  ////Uncomment for local development only
+  // server: {
+  //   watch: {
+  //     usePolling: true,
+  //   },
+  // },
+
+  resolve:{
+    "@": path.resolve(__dirname, "./src"),
+  }
 })
