e4365f525f80beb63d25c8d9d1eb2567f03117f1
gbudjeakp <gbudjeakp@gmail.com>
2024-06-06 23:48:11 -0500

    Fix deploy for BE

--- deploy-server.js
+++ deploy-server.js
@@ -5,28 +5,18 @@ const ftpDeploy = new FtpDeploy();
 const config = {
   user: process.env.FTP_USER,
   password: process.env.FTP_PASSWORD,
-  host: process.env.FTP_HOST,
+  host:  process.env.FTP_HOST,
   port: 21,
   localRoot: path.join(__dirname),
-  remoteRoot: './public_html/building-u-feedback-api', 
+  remoteRoot: '/building-u-feedback-api', 
   include: ['*', '**/*'], 
   exclude: ['.git', '.github', 'views/**', 'node_modules/**', 'deploy-backend.js', '.env'],
   deleteRemote: false,
   forcePasv: true,
+  sftp: false
 };
-// Add detailed logging to track the deployment process
-ftpDeploy.on('uploading', function(data) {
-    console.log(`Uploading ${data.transferredFileCount} of ${data.totalFilesCount}: ${data.filename}`);
-  });
-  
-  ftpDeploy.on('uploaded', function(data) {
-    console.log(`Uploaded: ${data.filename}`);
-  });
-  
-  ftpDeploy.on('upload-error', function(data) {
-    console.log('Error uploading file:', data.err);
-  });
-  
+
+
 ftpDeploy.deploy(config)
   .then(res => console.log('Finished:', res))
   .catch(err => console.error('Error:', err));
