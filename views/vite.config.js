253d937e626f778d3813dbbf73a8de5df73a09c5
buildingu <coding@building-U.com>
2024-06-25 01:01:57 -0500

    Add vite config

--- views/vite.config.js
+++ views/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Building-u-feedback/', // Add this line
 
   ////Uncomment for local development only
   // server: {
