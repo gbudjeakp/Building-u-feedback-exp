2714b93716c17042d9bc57fd93aed751d226a217
buildingu <coding@building-U.com>
2024-06-25 01:39:50 -0500

    Another fix attempt

--- views/src/App.jsx
+++ views/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Loginpage from "../src/Pages/LoginPage";
 import Signup from "./Pages/Signup";
 import Interndashboard from "./Pages/Interndashboard";
@@ -11,20 +11,21 @@ import "@mantine/core/styles.css";
 import { MantineProvider } from "@mantine/core";
 import AuthWrapper from "./Utility/AuthWrapper";
 
-
 function App() {
   return (
     <MantineProvider>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/login" element={<Loginpage />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/intern/*" element={<AuthWrapper>{({ user }) => <Interndashboard user={user} />}</AuthWrapper>} />
-        <Route path="/mentor/*" element={<AuthWrapper>{({ user }) => <Mentordashboard user={user} />}</AuthWrapper>} />
-        <Route path="/feedback/:id" element={<AuthWrapper>{({ user }) => <SingleFeedBack user={user} />}</AuthWrapper>} />
-        <Route path="/403" element={<Unauthorized />} />
-        <Route path="/404" element={<Notfound />} />
-      </Routes>
+      <Router basename="/Building-u-feedback">
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/login" element={<Loginpage />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/intern/*" element={<AuthWrapper>{({ user }) => <Interndashboard user={user} />}</AuthWrapper>} />
+          <Route path="/mentor/*" element={<AuthWrapper>{({ user }) => <Mentordashboard user={user} />}</AuthWrapper>} />
+          <Route path="/feedback/:id" element={<AuthWrapper>{({ user }) => <SingleFeedBack user={user} />}</AuthWrapper>} />
+          <Route path="/403" element={<Unauthorized />} />
+          <Route path="/404" element={<Notfound />} />
+        </Routes>
+      </Router>
     </MantineProvider>
   );
 }
