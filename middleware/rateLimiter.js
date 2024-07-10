c513363932639d5d5bf549eb1eecca1990eaae8a
buildingu <coding@building-U.com>
2024-07-09 23:22:57 -0500

    Various Fixes

--- middleware/rateLimiter.js
+++ middleware/rateLimiter.js
@@ -4,7 +4,7 @@ const limiter = require("express-rate-limit");
 //Need to find appropriate windows 1min might be too short.
 const rateLimiter = limiter({
   windowMs: 1 * 60 * 1000,
-  max: 4,
+  max: 150,
   message: "Too many requests, please try again later.",
 });
 
