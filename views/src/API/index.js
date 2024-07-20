44ac2cc5b26ab7d69539b2101c3200ea6a45675c
buildingu <coding@building-U.com>
2024-07-19 19:04:38 -0500

    Gave up

--- views/src/API/index.js
+++ views/src/API/index.js
@@ -1,6 +1,6 @@
 const getApiEndpoint = () => {
     if (process.env.NODE_ENV === 'production') {
-      return import.meta.env.VITE_API_ENDPOINT_PROD;
+      return "https://your-production-api-endpoint.com";
     } else {
       return import.meta.env.VITE_API_ENDPOINT_DEV;
     }
