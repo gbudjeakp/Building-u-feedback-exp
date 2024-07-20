2f5e4877b63f0339b3865784f5c142ca28f97428
buildingu <coding@building-U.com>
2024-07-19 19:14:19 -0500

    fix

--- views/src/API/index.js
+++ views/src/API/index.js
@@ -1,6 +1,8 @@
 const getApiEndpoint = () => {
+  //Could get env variables to work so just hard coding for now
+  //Will need to come back and fix this
     if (process.env.NODE_ENV === 'production') {
-      return "https://your-production-api-endpoint.com";
+      return "https://building-u-feedback-api.onrender.com";
     } else {
       return import.meta.env.VITE_API_ENDPOINT_DEV;
     }
