03f8cde1e72618aa265ec7843dcf6c862d42e96e
Sidragon123 <siddhanth.subramanian99@gmail.com>
2024-11-13 20:20:26 +0800

    ported new branch, fixed fetching logic on checklist.jsx, created scope for a loading screen incase the api call+unpackaging takes a while

--- views/src/Pages/Checklist.jsx
+++ views/src/Pages/Checklist.jsx
@@ -7,217 +7,96 @@ import {
   TableTr,
   TableTd,
 } from "@mantine/core";
+import { IconLoader } from "@tabler/icons-react";
+import axios from "axios";
 
-const stylingMainTopicsHeader = { textAlign: "center", width: "10%" };
-const stylingTopicStatusHeader = { width: "calc(25%/3)" };
-// const nameList = () => {
-//   for (internName in database) {
-//     return (
-//       <Table.Tr>
-//         <Table.Td>{internName}</Table.Td>
-//       </Table.Tr>
-//     );
-//   }
-// };
-// const statusExercises = ()=>{
-//   for(status){
-//     if (status.submittedhtml = true){
-//       return("x")
+// const getData = async () => {
+//   const response = await axios.get(
+//     "http://localhost:5001/api/users/exerciseInfo",
+//     {
+//       withCredentials: true,
 //     }
-//   }
-// }
+//   );
+//   return response;
+// };
 const Checklist = () => {
+  const [isLoading, setisLoading] = useState(true);
+  const [data, setData] = useState(null);
+  const [users, setUsers] = useState(null);
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:5001/api/users/exerciseInfo",
+          {
+            withCredentials: true,
+          }
+        );
+        console.log(response.data.data);
+        setData(response.data.data);
+        setisLoading(false);
+      } catch {
+        (error) => {
+          setisLoading(false);
+          console.log(error);
+        };
+      }
+    };
+    const getUsers = async () => {
+      try {
+        const responseUsers = await axios.get(
+          "http://localhost:5001/api/users/users",
+          {
+            withCredentials: true,
+          }
+        );
+        console.log(responseUsers);
+        setUsers(responseUsers);
+      } catch {
+        (error) => {
+          console.log(error);
+        };
+      }
+    };
+    getData();
+    getUsers();
+  }, []);
+  const dataUnpackager = (data) => {
+    for (element in data) {
+    }
+  };
+
   const stylingTable = {
     display: "flex",
     alignItems: "center",
     justifyContent: "center",
     height: "100vh",
   };
+  const LoadingScreen = () => {
+    return (
+      <div>
+        <IconLoader
+          style={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            width: "120px",
+            height: "120px",
+            margin: "-60px 0 0 -60px",
+            animation: "spin 4s linear infinite",
+          }}
+        />
+      </div>
+    );
+  };
+  const ChecklistTable = () => {
+    <ScrollArea w="75vw" h="80vh">
+      <Table></Table>
+    </ScrollArea>;
+  };
   return (
     <Container style={stylingTable}>
-      <ScrollArea w="75vw" h="80vh">
-        <Container style={{ display: "flex" }}>
-          <Table
-            highlightOnHover={true}
-            striped={true}
-            stickyHeader={true}
-            withTableBorder={true}
-            withColumnBorders={true}
-            withRowBorders={true}
-            w="25%"
-          >
-            <Table.Th
-              style={{
-                textAlign: "center",
-                width: "25%",
-                height: "200%",
-                alignContent: "center",
-              }}
-            >
-              Intern Name
-            </Table.Th>
-          </Table>
-          <Container style={{ display: "block" }}>
-            <Table
-              highlightOnHover={true}
-              striped={true}
-              stickyHeader={true}
-              withTableBorder={true}
-              w="100%"
-              withColumnBorders={true}
-              withRowBorders={true}
-            >
-              <Table.Thead w="100%">
-                <Table.Tr w="100%">
-                  <Table.Th style={stylingMainTopicsHeader}>HTML</Table.Th>
-                  <Table.Th style={stylingMainTopicsHeader}>CSS</Table.Th>
-                  <Table.Th style={stylingMainTopicsHeader}>MQ/RD</Table.Th>
-                  <Table.Th style={stylingMainTopicsHeader}>Bootstrap</Table.Th>
-                  <Table.Th style={stylingMainTopicsHeader}>
-                    Javascript
-                  </Table.Th>
-                  <Table.Th style={stylingMainTopicsHeader}>
-                    Javascript and the DOM
-                  </Table.Th>
-                  <Table.Th style={stylingMainTopicsHeader}>React</Table.Th>
-                  <Table.Th style={stylingMainTopicsHeader}>SQL</Table.Th>
-                  <Table.Th style={stylingMainTopicsHeader}>ExpressJS</Table.Th>
-                  <Table.Th style={stylingMainTopicsHeader}>
-                    API Integration
-                  </Table.Th>
-                </Table.Tr>
-              </Table.Thead>
-            </Table>
-            <Table
-              highlightOnHover={true}
-              striped={true}
-              stickyHeader={true}
-              w="100%"
-              withTableBorder={true}
-              withColumnBorders={true}
-              withRowBorders={true}
-            >
-              <Table.Thead>
-                <Table.Tr>
-                  <Table.Th id="htmlSubmitted" style={stylingTopicStatusHeader}>
-                    Submitted
-                  </Table.Th>
-                  <Table.Th id="htmlCompleted" style={stylingTopicStatusHeader}>
-                    Completed
-                  </Table.Th>
-                  <Table.Th id="cssSubmitted" style={stylingTopicStatusHeader}>
-                    Submitted
-                  </Table.Th>
-                  <Table.Th id="cssCompleted" style={stylingTopicStatusHeader}>
-                    Completed
-                  </Table.Th>
-                  <Table.Th id="mdrqSubmitted" style={stylingTopicStatusHeader}>
-                    Submitted
-                  </Table.Th>
-                  <Table.Th id="mdrqCompleted" style={stylingTopicStatusHeader}>
-                    Completed
-                  </Table.Th>
-                  <Table.Th
-                    id="bootstrapSubmitted"
-                    style={stylingTopicStatusHeader}
-                  >
-                    Submitted
-                  </Table.Th>
-                  <Table.Th
-                    id="bootstrapCompleted"
-                    style={stylingTopicStatusHeader}
-                  >
-                    Completed
-                  </Table.Th>
-                  <Table.Th id="jsSubmitted" style={stylingTopicStatusHeader}>
-                    Submitted
-                  </Table.Th>
-                  <Table.Th id="jsCompleted" style={stylingTopicStatusHeader}>
-                    Completed
-                  </Table.Th>
-                  <Table.Th
-                    id="jsDomSubmitted"
-                    style={stylingTopicStatusHeader}
-                  >
-                    Submitted
-                  </Table.Th>
-                  <Table.Th
-                    id="jsDomCompleted"
-                    style={stylingTopicStatusHeader}
-                  >
-                    Completed
-                  </Table.Th>
-                  <Table.Th
-                    id="reactSubmitted"
-                    style={stylingTopicStatusHeader}
-                  >
-                    Submitted
-                  </Table.Th>
-                  <Table.Th
-                    id="reactCompleted"
-                    style={stylingTopicStatusHeader}
-                  >
-                    Completed
-                  </Table.Th>
-                  <Table.Th id="sqlSubmitted" style={stylingTopicStatusHeader}>
-                    Submitted
-                  </Table.Th>
-                  <Table.Th id="sqlCompleted" style={stylingTopicStatusHeader}>
-                    Completed
-                  </Table.Th>
-                  <Table.Th
-                    id="expressJsSubmitted"
-                    style={stylingTopicStatusHeader}
-                  >
-                    Submitted
-                  </Table.Th>
-                  <Table.Th
-                    id="expressJsCompleted"
-                    style={stylingTopicStatusHeader}
-                  >
-                    Completed
-                  </Table.Th>
-                  <Table.Th
-                    id="apiIntegrationSubmitted"
-                    style={stylingTopicStatusHeader}
-                  >
-                    Submitted
-                  </Table.Th>
-                  <Table.Th
-                    id="apiIntegrationCompleted"
-                    style={stylingTopicStatusHeader}
-                  >
-                    Completed
-                  </Table.Th>
-                </Table.Tr>
-              </Table.Thead>
-              <Table.Tbody></Table.Tbody>
-            </Table>
-          </Container>
-        </Container>
-        <Container style={{ display: "flex" }}>
-          <Table
-            highlightOnHover={true}
-            striped={true}
-            stickyHeader={true}
-            withTableBorder={true}
-            withColumnBorders={true}
-            withRowBorders={true}
-          >
-            {/* {nameList()} */}
-          </Table>
-          <Table
-            highlightOnHover={true}
-            striped={true}
-            stickyHeader={true}
-            withTableBorder={true}
-            withColumnBorders={true}
-            withRowBorders={true}
-          >
-            {/* {statusExercises()} */}
-          </Table>
-        </Container>
-      </ScrollArea>
+      {isLoading ? <LoadingScreen /> : <ChecklistTable />}
     </Container>
   );
 };
