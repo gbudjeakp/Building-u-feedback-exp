e912d030ce3be82d6bb8b1f9d4c761604deaa671
Sidragon123 <siddhanth.subramanian99@gmail.com>
2024-11-15 22:49:11 +0800

    Loader

--- views/src/components/Loader.jsx
+++ views/src/components/Loader.jsx
@@ -1,34 +1,66 @@
-import { useEffect } from "react";
-
-const keyframes = [
+import { useEffect, useRef } from "react";
+const styleLoaderCircle = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  width: "90px",
+  height: "90px",
+  margin: "-60px 0 0 -60px",
+  border: "5px solid #F9EB02",
+  borderRadius: "50%",
+};
+const keyframes1 = [
   { transform: "scale(0)" },
+  { transform: "scale(0.166)" },
+  { transform: "scale(0.333)" },
+  { transform: "scale(0.5)" },
+  { transform: "scale(0.666)" },
+  { transform: "scale(0.833)" },
   { transform: "scale(1)" },
+  //   { transform: "opacity(0)" },
+];
+const timing = { duration: 1250, iterations: Infinity };
+const keyframes2 = [
+  { transform: "scale(0)" },
+  { transform: "scale(0)" },
+  { transform: "scale(0.25)" },
+  { transform: "scale(0.5)" },
+  { transform: "scale(0.75)" },
+  { transform: "scale(1)" },
+  //   { transform: "opacity(0)" },
+];
+
+const keyframes3 = [
+  { transform: "scale(0)" },
+  { transform: "scale(0)" },
+  { transform: "scale(0)" },
+  { transform: "scale(0.33)" },
+  { transform: "scale(0.66)" },
   { transform: "scale(1)" },
 ];
-const timing = { duration: 750, iterations: Infinity };
 const LoadIcon = () => {
+  const loadIconRef1 = useRef(null);
+  const loadIconRef2 = useRef(null);
+  const loadIconRef3 = useRef(null);
+  useEffect(() => {
+    loadIconRef1.current.animate(keyframes1, timing);
+  }, []);
+  useEffect(() => {
+    loadIconRef2.current.animate(keyframes2, timing);
+  }, []);
+  useEffect(() => {
+    loadIconRef3.current.animate(keyframes3, timing);
+  }, []);
   return (
-    <div
-      style={{
-        position: "absolute",
-        top: "50%",
-        left: "50%",
-        width: "120px",
-        height: "120px",
-        margin: "-60px 0 0 -60px",
-        border: "5px solid #F9EB02",
-        borderRadius: "50%",
-        opacity: "0",
-        animation: "scaleIn 4s infinite cubic-bezier(.36, .11, .89, .32)",
-      }}
-    ></div>
+    <>
+      <div ref={loadIconRef1} style={styleLoaderCircle}></div>
+      <div ref={loadIconRef2} style={styleLoaderCircle}></div>
+      <div ref={loadIconRef3} style={styleLoaderCircle}></div>
+    </>
   );
 };
 
 const Loader = () => {
-  //   useEffect(() => {
-  //     LoadIcon.animate(keyframes, timing);
-  //   }, []);
   return <LoadIcon />;
 };
 
